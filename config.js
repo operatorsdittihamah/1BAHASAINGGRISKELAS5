cf163666a7e4476073276e93f8aeebf7d656c11b
operatorsdittihamah <operatorsdittihamah@gmail.com>
2026-05-18 11:03:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const WEB_APP_URL =
-"https://script.google.com/macros/s/AKfycbw_GkSqcm66nZuM-NuNLm0_FeQJKkQpi-qCNVGZoi8iehiK43ObML4Ijo_pLTaqCBHy/exec";
+"https://script.google.com/macros/s/AKfycbxiKCpSHuozx4SmXheJOMLmfmeTeW5Y_nYgLw3zD3XwP3vceWLbK10Io7Ug5zYko9On/exec";
