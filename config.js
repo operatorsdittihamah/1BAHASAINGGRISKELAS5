dca1857031a9e50d06ebd0151ae98f4334e6cbf9
operatorsdittihamah <operatorsdittihamah@gmail.com>
2026-05-18 11:10:48 +0700

    OKOK

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const WEB_APP_URL =
-"https://script.google.com/macros/s/AKfycby7QCu145kKTNPUSMOHUprx46R1zoahwQkUJmsSUOgCEq6LtpGL3nhgoE1X6vYTnFXd/exec";
+"https://script.google.com/macros/s/AKfycbz04Xe5teC4uF06hthYiLmjwyaQ8eKvXXaBglu2YWpD-5Eof198VVpd2KWU39OzFX0X/exec";
